00992768e7380506003972dd3ac706059ddbff53
JuanJPernia <juanpernia0611@gmail.com>
2026-01-17 19:18:54 -0400

    Added features section

--- script.js
+++ script.js
@@ -43,3 +43,16 @@ document.addEventListener("DOMContentLoaded", () => {
     hero.style.transform = "translateY(0)";
   }, 100);
 });
+
+const cards = document.querySelectorAll(".feature-card");
+
+cards.forEach((card, index) => {
+  card.style.opacity = "0";
+  card.style.transform = "translateY(20px)";
+
+  setTimeout(() => {
+    card.style.transition = "all 0.6s ease";
+    card.style.opacity = "1";
+    card.style.transform = "translateY(0)";
+  }, index * 150);
+});
